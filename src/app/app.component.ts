e989248a002e314c991d4f96ad625c23d299015d
danidaw22 <danidaw2019@gmail.com>
2021-03-29 17:32:05 +0200

    Version2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'linkbio';
+
+
+title = "Mi link bio"
+darkMode = false;
+
+myRedes = [
+  {name:"twitch", image: "twitch.svg", enlace:"https://www.twitch.tv/dannylm22"},
+  {name:"github", image: "github.svg", enlace:"https://github.com/danidaw22"},
+  {name:"discord", image: "discord.svg", enlace:"https://discord.gg/K7hgCYQc"},
+  {name:"twitter", image: "twitter.svg", enlace:"https://twitter.com/DanielLandete"}
+]
+
+changeMode(data: any){
+
+  this.darkMode = !this.darkMode;
+
+  if(this.darkMode){
+    data.src="assets/images/light.png"
+  }else{
+    data.src="assets/images/dark-light.svg"
+  }
+}
+
 }
